c4acdae07c11720fef3ff8f8da08ec955d37651d
susa.keyber <susa@keyber.jp>
2022-06-07 06:41:55 +0900

    📦️ upgrade:  Isar package を追加

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -63,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
