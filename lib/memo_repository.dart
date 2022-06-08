b1780c8f35559c314fa644a1a80569b3e5d0db26
susa.keyber <susa@keyber.jp>
2022-06-08 16:14:42 +0900

    🚀 feat:  初期データ投入

--- lib/memo_repository.dart
+++ lib/memo_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:flutter/services.dart';
 import 'package:isar/isar.dart';
 
 import 'collections/category.dart';
@@ -11,11 +13,53 @@ import 'collections/memo.dart';
 class MemoRepository {
   MemoRepository(this.isar) {
     Future(() async {
-      // 初回起動時にカテゴリの初期値を追加
+      // （デバッグ用）データを全削除する
+      await isar.writeTxn((isar) async {
+        final categories = await isar.categorys.where().findAll();
+        await isar.categorys.deleteAll(categories.map((e) => e.id).toList());
+        final memos = await isar.memos.where().findAll();
+        await isar.memos.deleteAll(memos.map((e) => e.id).toList());
+      });
+
+      // 初回起動時に初期データをDBに書き込む
       if (await isar.categorys.count() == 0) {
         await isar.writeTxn((isar) async {
-          for (final seed in ['仕事', 'プライベート', 'その他']) {
-            await isar.categorys.put(Category()..name = seed);
+          // カテゴリの初期データ
+          await isar.categorys.putAll(
+            ['仕事', 'プライベート', 'その他']
+                .map((name) => Category()..name = name)
+                .toList(),
+          );
+        });
+        await isar.writeTxn((isar) async {
+          final categories = await isar.categorys.where().findAll();
+
+          // メモの初期データはJSONから取ってくる
+          final bytes = await rootBundle.load('assets/json/seed_memos.json');
+          final jsonString =
+              const Utf8Decoder().convert(bytes.buffer.asUint8List());
+          final jsonArray = json.decode(jsonString) as List;
+
+          final now = DateTime.now();
+          final memos = <Memo>[];
+          for (final jsonMap in jsonArray) {
+            if (jsonMap is Map<String, dynamic>) {
+              memos.add(
+                Memo()
+                  ..category.value = categories.firstWhere(
+                    (category) => category.id == jsonMap['categoryId'] as int,
+                  )
+                  ..content = jsonMap['content'] as String
+                  ..createdAt = now
+                  ..updatedAt = now,
+              );
+            }
+          }
+
+          await isar.memos.putAll(memos);
+          final saveCategories = memos.map((memo) => memo.category).toList();
+          for (final saveCategory in saveCategories) {
+            await saveCategory.save();
           }
         });
       }
